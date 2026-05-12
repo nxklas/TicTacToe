db446a1d5a72507acc4638d509b5d3180b302d8f
nxklas <51093729+nxklas@users.noreply.github.com>
2026-05-12 22:18:50 +0200

    Add board rendering logic

--- src/program.c
+++ src/program.c
@@ -1,5 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum CellState {
+    CELL_STATE_O = -1,
+    CELL_STATE_NONE = 0,
+    CELL_STATE_X = 1,
+} CellState;
+
+enum BoardDimensions {
+    BOARD_SIZE = 3,
+    BOARD_TOTAL_SIZE = BOARD_SIZE * BOARD_SIZE,
+};
+
+typedef struct Board {
+    CellState cells[BOARD_TOTAL_SIZE];
+} Board;
+
+void init_board(Board* board) {
+    for (int i = 0; i < BOARD_TOTAL_SIZE; i++)
+        board->cells[i] = CELL_STATE_NONE;
+}
+
+Board* create_board(void) {
+    Board* board = malloc(sizeof(Board));
+    if (!board) {
+        printf("No memory could be allocated to store the board in.\n");
+        return NULL;
+    }
+    init_board(board);
+    return board;
+}
+
+void render_board(const Board* board)
+{
+    #define STR_LEN (BOARD_SIZE * ((BOARD_SIZE * 4) - 2))
+    char str[STR_LEN + 1];
+    int str_i = 0;
+
+    for (int i = 0; i < BOARD_TOTAL_SIZE; i++)
+    {
+        const CellState state = board->cells[i];
+        if (state == CELL_STATE_X)
+            str[str_i++] = 'X';
+        else if (state == CELL_STATE_O)
+            str[str_i++] = 'O';
+        else
+            str[str_i++] = ' ';
+        
+        if ((i + 1) % BOARD_SIZE == 0)
+        {
+            str[str_i++] = '\n';
+            continue;
+        }
+
+        str[str_i++] = ' ';
+        str[str_i++] = '|';
+        str[str_i++] = ' ';
+    }
+
+    str[str_i] = '\0';
+    printf("%s", str);
+}
 
 int main(void) {
-    printf("Hello, world!\n");
+    Board* board = create_board();
+    if (!board)
+        return 1;
+    render_board(board);
 }
